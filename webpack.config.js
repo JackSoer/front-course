189b988a1f79b4ad50bc8f4a6c8c1c3908f03c48
JackSoer <kirillrotko1@gmail.com>
2024-03-24 16:46:29 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       title: "webpack Boilerplate",
-      template: path.resolve(__dirname, "index.html"),
+      template: path.resolve(__dirname, "./src/pages/index.html"),
       filename: "index.html",
     }),
     new CleanWebpackPlugin(),
